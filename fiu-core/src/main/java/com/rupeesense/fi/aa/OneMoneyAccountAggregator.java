3c8337985f77b76d586e2704ffa0193aba5d7eaf
Aviral Verma <aviral1701@gmail.com>
2023-06-30 12:17:49 +0530

    add exception for client

--- fiu-core/src/main/java/com/rupeesense/fi/aa/OneMoneyAccountAggregator.java
+++ fiu-core/src/main/java/com/rupeesense/fi/aa/OneMoneyAccountAggregator.java
@@ -2,12 +2,17 @@ package com.rupeesense.fi.aa;
 
 import static com.rupeesense.fi.CoreModule.ONE_MONEY_CLIENT_NAME;
 
+import com.rupeesense.fi.aa.exception.AAClientException;
+import com.rupeesense.fi.aa.exception.AAServerException;
 import com.rupeesense.fi.ext.onemoney.request.OneMoneyConsentAPIRequest;
 import com.rupeesense.fi.ext.onemoney.response.OneMoneyConsentAPIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.bind.Name;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class OneMoneyAccountAggregator {
@@ -19,13 +24,27 @@ public class OneMoneyAccountAggregator {
     this.webClient = webClient;
   }
 
-  //TODO: add error handling
   public OneMoneyConsentAPIResponse initiateConsent(OneMoneyConsentAPIRequest request) {
     return webClient.post()
         .uri("/aa/consent")
         .bodyValue(request)
         .retrieve()
+        .onStatus(HttpStatus::isError, this::createException)
         .bodyToMono(OneMoneyConsentAPIResponse.class)
         .block();
+  }
+
+  public Mono<Throwable> createException(ClientResponse clientResponse) {
+    return clientResponse.bodyToMono(String.class)
+        .flatMap(errorMessage -> {
+          if (clientResponse.statusCode().is4xxClientError()) {
+            return Mono.error(new AAClientException(errorMessage));
+          } else if (clientResponse.statusCode().is5xxServerError()) {
+            return Mono.error(new AAServerException(errorMessage));
+          } else {
+            return Mono.error(new RuntimeException("Unexpected status code: " +
+                clientResponse.statusCode() + " AA: " + this.getClass().getName()));
+          }
+        });
   }
 }
---
+++ fiu-core/src/main/java/com/rupeesense/fi/aa/exception/AAServerException.java
@@ -0,0 +1,8 @@
+package com.rupeesense.fi.aa.exception;
+
+public class AAServerException extends Exception {
+
+  public AAServerException(String message) {
+    super(message);
+  }
+}
