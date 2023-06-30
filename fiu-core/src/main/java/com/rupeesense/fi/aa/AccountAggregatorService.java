5f1013bed0daf075a93825af4d2a6b6709161a49
Aviral Verma <aviral1701@gmail.com>
2023-06-30 12:21:52 +0530

    Update AccountAggregatorService.java

--- fiu-core/src/main/java/com/rupeesense/fi/aa/AccountAggregatorService.java
+++ fiu-core/src/main/java/com/rupeesense/fi/aa/AccountAggregatorService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AccountAggregatorService {
 
-  OneMoneyAccountAggregator oneMoneyAccountAggregator;
+  private final OneMoneyAccountAggregator oneMoneyAccountAggregator;
 
   @Autowired
   public AccountAggregatorService(OneMoneyAccountAggregator oneMoneyAccountAggregator) {
