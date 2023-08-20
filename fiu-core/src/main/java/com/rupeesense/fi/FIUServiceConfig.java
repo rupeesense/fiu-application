4c36b675ff5b170aec17f918dfcbc4464e6624bd
Aviral Verma <aviral1701@gmail.com>
2023-08-20 15:04:32 +0530

    pushed

--- fiu-core/src/main/java/com/rupeesense/fi/FIUServiceConfig.java
+++ fiu-core/src/main/java/com/rupeesense/fi/FIUServiceConfig.java
@@ -3,6 +3,7 @@ package com.rupeesense.fi;
 import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -14,5 +15,15 @@ public class FIUServiceConfig {
 
   @NotBlank
   private String fiuId;
+
+  @NotBlank
+  private String setuURI;
+
+  @NotBlank
+  private String setuClientId;
+
+  @Value("${sm://setu-client-secret}")
+  @NotBlank
+  private String setuClientSecret;
 
 }
