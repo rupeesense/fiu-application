01e8d3ad187a55c059df12d08f91c3f2e3fc2fe7
Aviral Verma <aviral1701@gmail.com>
2023-08-20 12:35:50 +0530

    updates

--- fiu-core/src/main/java/com/rupeesense/fi/CoreModule.java
+++ fiu-core/src/main/java/com/rupeesense/fi/CoreModule.java
@@ -1,21 +1,28 @@
 package com.rupeesense.fi;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.channel.ChannelOption;
+import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class CoreModule {
-
-  public static final String ONE_MONEY_CLIENT_NAME = "one-money-web-client";
 
   public static final String SETU_CLIENT_NAME = "setu-web-client";
 
   @Bean(name = SETU_CLIENT_NAME)
   public WebClient getSetuClient() {
+    HttpClient client = HttpClient.create()
+        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+        .responseTimeout(Duration.ofSeconds(12));
+
     return WebClient
         .builder()
+        .clientConnector(new ReactorClientHttpConnector(client))
         .baseUrl("https://fiu-uat.setu.co")
         .defaultHeader("Content-Type", "application/json")
         .defaultHeader("x-client-id", "f26b25b8-8e33-4c86-a28e-4575ddeeb09a")
@@ -25,7 +32,7 @@ public class CoreModule {
 
   @Bean
   public ObjectMapper getObjectMapper() {
-    ObjectMapper objectMapper =  new ObjectMapper();
+    ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.findAndRegisterModules();
     return objectMapper;
   }
